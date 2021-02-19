815a9c02ef305e3629f1cb77b8f3f5d11a26a285
Paul Brack <paul.brack@manchester.ac.uk>
2021-02-19 13:02:13 +0000

    Add .NET Core solution

---
+++ RO-Crate validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RO_Crate_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
